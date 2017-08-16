fc6cffcfe9064446bc6161aa1c23b574413b5600
Victor Pignataro <victorpignataro@msn.com>
2017-08-16 16:46:47 -0300

    Postando a pasta Aplicacao
    pasta que contem a interface gráfica

---
+++ Aplicacao/src/condominiosystem/main.java
@@ -0,0 +1,23 @@
+package condominiosystem;
+
+
+
+public class main {
+
+    public static void main(String[] args) {
+    
+       Tela_inicial tela = new Tela_inicial();
+
+       tela.setVisible(true);
+       
+       
+       
+       
+        
+    }
+    
+    
+    
+    
+
+}
