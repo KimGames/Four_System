767873f81d97d085d9d395acdae6394ea2c0d4b0
KimGames <kimgames1@gmail.com>
2017-08-17 14:09:20 +0000

    mudanças

--- Four_System/src/Modulos/Modulo_insercoes/InserePessoa.java
+++ Four_System/src/Modulos/Modulo_insercoes/InserePessoa.java
@@ -1,11 +1,12 @@
 
-import Classes.Morador;
+import Classes.Pessoa;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class InserePessoa{
 
-  Statement sentenca;
+  private Statement sentenca;
+
   public InserePessoa(){
       Conexao conexao = new Conexao();
       sentenca = conexao.getStatement();
@@ -20,13 +21,14 @@ public class InserePessoa{
       query = "INSERT INTO pessoa "
             + "VALUES (DEFAULT, '" + cpf + "', '" + nome + "');";
       sentenca.execute(query);
+      insercao = 1;
     }catch(SQLException se){
       System.out.println(">Nao foi possivel realizar a insercao!");
     }
     return insercao;
   }
 
-  public int insrirMorador(Morador morador){
+  public int insrirMorador(Pessoa morador){
 
     String id_morador = morador.getId();
     int insercao = 0;
@@ -36,6 +38,7 @@ public class InserePessoa{
       query = "INSERT INTO tipo_pessoa "
             + "VALUES ('Morador', '" + id_morador + "');";
       sentenca.execute(query);
+      insercao = 1;
     }catch(SQLException se){
       System.out.println(">Nao foi possivel realizar a insercao!");
     }
