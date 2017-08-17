8099bfa0f53dd4a99f0c2808c3077260e82a11ee
kimgames <kimgames1@gmail.com>
2017-08-17 03:10:52 +0000

    mudança projeto

--- Four_System/src/Interface Grafica/Interface/main.java
+++ Four_System/src/Interface Grafica/Interface/main.java
@@ -5,19 +5,9 @@ package condominiosystem;
 public class main {
 
     public static void main(String[] args) {
-    
+
        Tela_inicial tela = new Tela_inicial();
 
        tela.setVisible(true);
-       
-       
-       
-       
-        
     }
-    
-    
-    
-    
-
 }
